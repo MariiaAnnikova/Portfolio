198eb9ba74525303ab0ca8fd5673a4f92be94ed6
MariiaAnnikova <radostu@gmail.com>
2023-07-25 14:14:45 +0300

    portfolio_block

--- src/constants/portfolio.js
+++ src/constants/portfolio.js
@@ -1,7 +1,48 @@
+// import ImgOne from './images/Logo.png'
+// import ImgTwo from './images/Sushi.jpeg'
+import ImgTree from './images/Image.png'
+import ImgFour from './images/Image4.png'
+import ImgFive from './images/Image5.png'
+import ImgSix from './images/Image6.png'
+import ImgSeven from './images/Image7.png'
+
+
 export const CATEGORIES = ['all', 'React', 'Html_and_Css', 'Redux'];
 
-export const PORTFOLIO_DATA = [{
-    img: '',
+ export const PORTFOLIO_DATA = [
+    //{
+//     img: ImgOne,
+//     link: '/',
+//     category: CATEGORIES[1]
+// },
+// {
+//     img: ImgTwo,
+//     link: '/',
+//     category: CATEGORIES[2]
+// },
+{
+    img: ImgTree,
     link: '/',
     category: CATEGORIES[1]
-}]
+},
+{
+    img: ImgFour,
+    link: '/',
+    category: CATEGORIES[3]
+},
+{
+    img: ImgFive,
+    link: '/',
+    category: CATEGORIES[1]
+},
+{
+    img: ImgSix,
+    link: '/',
+    category: CATEGORIES[2]
+},
+{
+    img: ImgSeven,
+    link: '/',
+    category: CATEGORIES[3]
+},
+]
---
+++ src/shared/portfolioCard/portfolioCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+ import './portfolioCard.css'
+export const PortfolioCard = ({ data }) => 
+    <div className='portfolio_card'>
+        <a href={data.link}>
+            <img src={data.img} alt={data.img} />
+        </a>
+    </div>
+ 
